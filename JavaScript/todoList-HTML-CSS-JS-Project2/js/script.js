9b20a2af3a0d55fc6d8b6df9ffc3d9e6a4096019
Hagar <hagar.khaled.niazi@gmail.com>
2026-08-23 21:49:00 +0300

    Create To-Do List project structure

---
+++ JavaScript/todoList-HTML-CSS-JS-Project2/js/script.js
@@ -0,0 +1,34 @@
+            // Select Elements
+
+
+            // Variables
+
+
+            // Tasks Array
+
+
+            // Add Task
+
+
+            // Display Tasks
+
+
+            // Delete Task
+
+
+            // Edit Task
+
+
+            // Complete Task
+
+
+            // Filter Tasks
+
+
+            // Clear Completed
+
+
+            // Update Tasks Counter
+
+
+            // Local Storage
